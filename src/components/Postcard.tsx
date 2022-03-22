c07001ee753a6b074f638b6b29b3e3a79f71a538
kiko-g <kikojpgoncalves@gmail.com>
2022-03-22 19:38:50 +0000

    postcard style

--- src/components/Postcard.tsx
+++ src/components/Postcard.tsx
@@ -4,13 +4,16 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 export const Postcard = ({ post }) => (
   <Link
-    className="h-auto w-full max-w-7xl space-y-2 rounded-xl bg-light3 p-3 shadow transition hover:bg-lilac/5 dark:bg-dark3 hover:dark:bg-lilac/20"
     to={post.frontmatter.slug}
+    className="group max-w-7xl space-y-2 rounded-xl bg-light1 p-3 shadow transition 
+    hover:bg-light3 hover:text-dark dark:bg-dark3 hover:dark:bg-lilac/20 dark:hover:text-white"
   >
     <div className="flex h-auto flex-col items-center justify-between rounded-lg bg-purple-300 bg-opacity-90 p-4 md:col-span-1" />
     <footer className="w-full">
       <h3 className="font-medium">{post.frontmatter.title}</h3>
-      <p className="text-sm font-normal text-gray-400 dark:text-gray-400">{post.excerpt}</p>
+      <p className="text-sm font-normal text-gray-400 transition group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-200">
+        {post.excerpt}
+      </p>
     </footer>
   </Link>
 )
