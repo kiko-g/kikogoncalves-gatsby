114eeb8f83c7ec2c030166388be2da76176109d4
kiko-g <kikojpgoncalves@gmail.com>
2022-03-26 17:53:21 +0000

    animated homepage

---
+++ src/layout/Background.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/animation.css'
+
+export const Background = () => {
+  return (
+    <div className="background">
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import { Background } from './Background'
 import { DarkModeSwitchSublime } from './DarkModeSwitchSublime'
 import { useStaticQuery, graphql } from 'gatsby'
 
@@ -10,15 +11,7 @@ type Props = {
   homepage?: boolean
 }
 
-function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export const Layout: React.FC<Props> = ({ children, location, homepage }) => {
-  /**
-   * Layout component that queries for data * with Gatsby's useStaticQuery component
-   * See: https://www.gatsbyjs.com/docs/use-static-query/
-   */
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -28,21 +21,20 @@ export const Layout: React.FC<Props> = ({ children, location, homepage }) => {
       }
     }
   `)
-  const title = data.site.siteMetadata?.title
 
   return (
     <>
       {homepage ? (
         <div className="home">
-          <div className="background" />
+          <Background />
           <div className="absolute top-4 right-4">
             <DarkModeSwitchSublime />
           </div>
-          <div>{children}</div>
+          {children}
         </div>
       ) : (
         <div className="layout">
-          <Navbar location={location} siteTitle={title} />
+          <Navbar location={location} siteTitle={data.site.siteMetadata?.title} />
           <div className="mx-auto my-auto max-w-screen">{children}</div>
           <Footer />
         </div>
