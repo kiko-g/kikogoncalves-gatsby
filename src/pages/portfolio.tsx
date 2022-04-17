78d041163d1a55db4bf192e472c9e5af740d6876
kiko-g <kikojpgoncalves@gmail.com>
2022-04-17 05:00:37 +0100

    add portfolio starter and styling refactor

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
-import { Layout } from '../layout/Layout'
+import { graphql } from 'gatsby'
 import Seo from '../components/Seo'
+import { Layout } from '../layout/Layout'
+import { ProjectCard } from '../components/ProjectCard'
+import '../styles/pages/portfolio.css'
 
-const PortfolioPage = () => (
+// prettier-ignore
+const PortfolioPage = ({ data: { allMarkdownRemark: { edges }, }, }) => (
   <Layout location="Portfolio">
     <Seo title="Portfolio" />
+    <main className="portfolio">
+      <header>
+        <h2>Portfolio</h2>
+        <p>Portfolio description</p>
+      </header>
+
+      <article>
+        {edges
+          .filter((edge: { node: { frontmatter: { date: any } } }) => !!edge.node.frontmatter.date)
+          .map((edge: { node: { id: React.Key } }) => (
+            <ProjectCard key={edge.node.id} post={edge.node} />
+          ))}
+      </article>
+    </main>
   </Layout>
 )
 
 export default PortfolioPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: [DESC, DESC], fields: [frontmatter___pinned, frontmatter___date] }
+      filter: { fileAbsolutePath: { regex: "/(portfolio)/" } }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 85)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+            pinned
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(width: 800, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
