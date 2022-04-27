8b788ee39bbf795c8e0f2a2bacfa493a1dc46a64
kiko-g <kikojpgoncalves@gmail.com>
2022-04-27 17:49:56 +0100

    feat: coming soon

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Layout } from '../layout/Layout'
 import Seo from '../components/Seo'
+import { Layout } from '../layout/Layout'
+import { ComingSoon } from '../layout/ComingSoon'
 import '../styles/cv.css'
 
 const CvPage = () => (
   <Layout location="CV">
     <Seo title="CV" />
+    <ComingSoon />
   </Layout>
 )
 
--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Seo from '../components/Seo'
 import { Layout } from '../layout/Layout'
+import { ComingSoon } from '../layout/ComingSoon'
 import '../styles/me.css'
 
 const MePage = () => {
   return (
-    <Layout location="Me" background={false}>
+    <Layout location="Me">
       <Seo title="Me" />
+      <ComingSoon />
     </Layout>
   )
 }
