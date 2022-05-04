064ff313b1a41ea86e53c5d5492c701ffa301955
kiko-g <kikojpgoncalves@gmail.com>
2022-05-04 22:13:09 +0100

    gallery uses gatsby image

--- src/components/hero/Gallery.tsx
+++ src/components/hero/Gallery.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image'
 
 const Gallery = () => {
-  const gallery = Array(10).fill(false)
+  const images = useStaticQuery(graphql`
+    query AssetsPhotos {
+      allFile(filter: { extension: { regex: "/(jpg)|(jpeg)|(png)/" }, relativeDirectory: { eq: "hero" } }) {
+        edges {
+          node {
+            id
+            name
+            img: childImageSharp {
+              gatsbyImageData(width: 300, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+            }
+          }
+        }
+      }
+    }
+  `).allFile.edges
 
   return (
     <div className="px-2 py-2">
       <div className="tab-inner-header">Gallery</div>
       <div className="mt-0 grid w-full grid-cols-2 gap-4 md:mt-2 lg:grid-cols-4 xl:grid-cols-5">
-        {gallery.map((image: string, imageIdx: number) => {
+        {images.map((imageGatsby: { node: { img: ImageDataLike } }, imageIdx: number) => {
           return (
-            <img
-              src={`/images/hero/hero${imageIdx}.jpg`}
+            <GatsbyImage
+              image={getImage(imageGatsby.node.img)}
               key={`gallery-${imageIdx}`}
               alt={`gallery-${imageIdx}`}
-              className="aspect-square h-full rounded-xl object-cover object-top shadow"
+              objectPosition="50% 0%"
+              className="aspect-square h-full rounded-xl object-cover shadow"
             />
           )
         })}
