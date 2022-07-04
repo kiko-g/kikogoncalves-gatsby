9a8a5205a0851decc36f58eb1ba28ca80f5690ed
kiko-g <kikojpgoncalves@gmail.com>
2022-07-04 01:19:03 +0100

    refactor: homepage styles organization

--- src/components/blog/BlogPostCard.tsx
+++ src/components/blog/BlogPostCard.tsx
@@ -3,41 +3,43 @@ import { Link } from 'gatsby'
 import { daysDifference } from '../../utils'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const BlogPostCard = ({ post }) => {
+type Props = {
+  post: any
+}
+
+const BlogPostCard = ({ post }: Props) => {
   const difference = daysDifference(post.frontmatter.date)
   const coverImage = getImage(post.frontmatter.featuredImage)
   const shown = difference >= 0
   const isNew = difference < 60
 
-  return (
-    shown && (
-      <Link to={post.frontmatter.slug} className="postcard group">
-        <header>
-          {coverImage ? (
-            <GatsbyImage image={coverImage} alt="cover" className="h-48 w-full rounded-lg object-contain" />
-          ) : (
-            <div className="h-48 w-full rounded-lg bg-gradient-to-br from-tertiary via-indigo-400 to-secondary shadow" />
-          )}
+  return shown ? (
+    <Link to={post.frontmatter.slug} className="postcard group">
+      <header>
+        {coverImage ? (
+          <GatsbyImage image={coverImage} alt="cover" className="h-48 w-full rounded-lg object-contain" />
+        ) : (
+          <div className="h-48 w-full rounded-lg bg-gradient-to-br from-tertiary via-indigo-400 to-secondary shadow" />
+        )}
 
-          {post.frontmatter.pinned && (
-            <span className="pinned">
-              <PinIcon />
-            </span>
-          )}
+        {post.frontmatter.pinned && (
+          <span className="pinned">
+            <PinIcon />
+          </span>
+        )}
 
-          <span className="date">{post.frontmatter.date}</span>
-        </header>
+        <span className="date">{post.frontmatter.date}</span>
+      </header>
 
-        <section>
-          <div className="top">
-            <h3 className="title">{post.frontmatter.title}</h3>
-            {isNew && <span className="new">New</span>}
-          </div>
-          <p className="excerpt">{post.excerpt}</p>
-        </section>
-      </Link>
-    )
-  )
+      <section>
+        <div className="top">
+          <h3 className="title">{post.frontmatter.title}</h3>
+          {isNew && <span className="new">New</span>}
+        </div>
+        <p className="excerpt">{post.excerpt}</p>
+      </section>
+    </Link>
+  ) : null
 }
 
 export default BlogPostCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,41 +21,48 @@ const IndexPage = () => {
   return (
     <Layout location="Home" liquid>
       <Seo title="Home" />
-      <div className="index-wrapper">
-        <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-          <Tab.List className="tab-list">
-            {headers.map(category => (
-              <Tab
-                key={category}
-                className={({ selected }) =>
-                  classNames('tab', focusRing ? 'tab-focus-ring' : '', selected ? 'tab-selected' : 'tab-not-selected')
-                }
-              >
-                {category}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="tab-panels">
-            {content.map((content: JSX.Element, index: number) => (
-              <Tab.Panel key={index} className={classNames('tab-panel', focusRing ? 'tab-focus-ring' : '')}>
-                {content}
-              </Tab.Panel>
-            ))}
-          </Tab.Panels>
-        </Tab.Group>
+      <main className="home">
+        <header>
+          <h2>Francisco Gonçalves</h2>
+          <p>Welcome to my website. Here you can find more about me. Take a look around! 😄</p>
+        </header>
 
-        <div className="see-more-wrapper">
-          <div className="see-more-left"></div>
-          <div className="see-more-right">
-            <Link to="/me" className="see-more-button">
-              <span>
-                <span>See more</span>
-                <ArrowNarrowRightIcon className="inline-flex h-5 w-5" />
-              </span>
-            </Link>
+        <div className="tabs">
+          <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+            <Tab.List className="tab-list">
+              {headers.map(category => (
+                <Tab
+                  key={category}
+                  className={({ selected }) =>
+                    classNames('tab', focusRing ? 'tab-focus-ring' : '', selected ? 'tab-selected' : 'tab-not-selected')
+                  }
+                >
+                  {category}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="tab-panels">
+              {content.map((content: JSX.Element, index: number) => (
+                <Tab.Panel key={index} className={classNames('tab-panel', focusRing ? 'tab-focus-ring' : '')}>
+                  {content}
+                </Tab.Panel>
+              ))}
+            </Tab.Panels>
+          </Tab.Group>
+
+          <div className="see-more-wrapper">
+            <div className="see-more-left"></div>
+            <div className="see-more-right">
+              <Link to="/me" className="see-more-button">
+                <span>
+                  <span>See more</span>
+                  <ArrowNarrowRightIcon className="inline-flex h-5 w-5" />
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </Layout>
   )
 }
