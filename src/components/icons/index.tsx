8840a2496745856f36f39b491a8ddb563f0fe37a
kiko-g <kikojpgoncalves@gmail.com>
2023-08-23 00:31:41 +0100

    fix: custom svg component icon props

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function LinkedinIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 512 512" aria-hidden="true">
+    <svg fill="currentColor" viewBox="0 0 512 512" aria-hidden="true" {...props}>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -22,7 +22,14 @@ export function GithubIcon(props: React.SVGProps<SVGSVGElement>) {
 
 export function PinIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
       <path
         fill="#fff"
         d="M13.554 2.662a2.25 2.25 0 013.055-.02l.125.124L21.5 7.85a2.251 2.251 0 01-.389 3.408l-.136.084-4.897 2.798a.75.75 0 00-.28.282l-.044.091-1.803 4.514a.75.75 0 01-1.147.322l-.08-.07-3.237-3.236-4.951 4.96L3.47 21l.002-1.055 4.953-4.962L5.22 11.78a.75.75 0 01.157-1.18l.095-.046 4.495-1.797a.75.75 0 00.328-.254l.056-.09 2.756-5.168a2.25 2.25 0 01.447-.583z"
@@ -40,6 +47,7 @@ export function DeployIcon(props: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       stroke="currentColor"
       strokeWidth={2}
+      {...props}
     >
       <path
         strokeLinecap="round"
@@ -57,6 +65,7 @@ export function PreviewIcon(props: React.SVGProps<SVGSVGElement>) {
       className="h-5 w-5"
       viewBox="0 0 20 20"
       fill="currentColor"
+      {...props}
     >
       <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
       <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
